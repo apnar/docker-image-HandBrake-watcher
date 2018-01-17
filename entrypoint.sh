7af728fcda685f1e9955691e26ba0578e0f4ccb6
Josh Lukens <jlukens@botch.com>
2018-01-16 21:38:48 -0500

    write title after conversion

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,5 +37,5 @@ inotifywait --recursive --monitor --quiet -e moved_to -e close_write --format '%
 
 FULL_FILE_NAME=$(echo ${INPUT_FILE##*/})
 OUTPUT_FILE=$(echo ${FULL_FILE_NAME%.*})
-tsp bash -c 'nice -n 19 /usr/bin/HandBrakeCLI -v -i "$0" -o "$1""$2"."$3" "$4" && if [[ "$5" = yes ]] ; then sleep 15 ; rm -f "$0" ; fi ; rmdir -p "$(dirname "$0")"' "$INPUT_FILE" "$OUTPUT_DIR" "$OUTPUT_FILE" "$MY_FILE_TYPE" "$HANDBRAKE_SETTINGS" "$MY_DELETE_SOURCE"
+tsp bash -c 'nice -n 19 /usr/bin/HandBrakeCLI -v -i "$0" -o "$1""$2"."$3" "$4" && mkvpropedit "$1""$2"."$3" --edit info --set \"title="$2"\" && if [[ "$5" = yes ]] ; then sleep 15 ; rm -f "$0" ; fi ; rmdir -p "$(dirname "$0")"' "$INPUT_FILE" "$OUTPUT_DIR" "$OUTPUT_FILE" "$MY_FILE_TYPE" "$HANDBRAKE_SETTINGS" "$MY_DELETE_SOURCE"
 done
